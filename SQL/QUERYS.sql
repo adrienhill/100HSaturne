7b42704ff59a345f4b98a6a7b7f51591d0b0ba15
arthurkl1 <arthurk1002@gmail.com>
2017-03-01 17:39:15 +0100

    V2.1
    Rajout du dossier SQL contenant les requêtes SQL

---
+++ SQL/QUERYS.sql
@@ -0,0 +1,4 @@
+INSERT INTO etudiant(mailEtudiant,mdpEtudiant)VALUES("arthur.klein@hei.yncrea.fr","motdepasse");
+INSERT INTO categorie(titreCategorie)VALUES("Rap");
+INSERT INTO publication(titrePublication,datePublication,lienPublication,idCategorie)VALUES("B20","1994/10/10","www.youtube.com/watch?v=iBo2-K25a6w","1");
+INSERT INTO Evenement(titreEvenement,lieuEvenement,datedebutEvenement,datefinEvenement,idCategorie)VALUES("soirée YNCREA","Belgium","10/10/1994","10/10/1994","1");
